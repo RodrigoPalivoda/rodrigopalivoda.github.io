92353b8d750af73f18008bc2e94c79054845e5a0
RodrigoPalivoda <RodrigoPalivoda@users.noreply.github.com>
2018-11-22 17:48:43 -0300

    from pcceibal

---
+++ rpg.js
@@ -0,0 +1,1 @@
+var juegosRpg = '[{"nombre":"Dragon Quest XI", "marca":"Square Enix", "descuento":"30", "antes":"2600", "ahora":"2000", "link":"dragonquest11.html", "imagen":"../media/dragonquest11-small.jpg"}]';
